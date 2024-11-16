bea9cd3fdb74739bca68f5697192fe610dc1ba54
Ebenezer <ebenirene5@gmail.com>
2024-11-16 13:20:52 +0100

    added dependencnies and  security config

---
+++ src/main/java/com/notePad/securedNotePad/SecuredNotePadApplication.java
@@ -0,0 +1,13 @@
+package com.notePad.securedNotePad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredNotePadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredNotePadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notePad/securedNotePad/SecuredNotePadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notePad.securedNotePad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredNotePadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
